0906c5e31dc69327e42e58bb62fd417d911534b4
Anuhitha13 <71086518+Anuhitha13@users.noreply.github.com>
2020-12-19 13:38:42 +0530

    Add files via upload

---
+++ arraytrig.py
@@ -0,0 +1,16 @@
+import math
+a = math.pi/6
+b = 3
+c = 4
+print('the value of sine of pi/6 is:',end=" ")
+print(math.sin(a))
+print('the value of cosine of pi/6 is :',end=" ")
+print(math.cos(a))
+print('the value of tangent of pi/6 is :',end=" ")
+print(math.tan(a))
+print('the value of hypotenuse of 3 and 4 is:',end=" ")
+print(math.hypot(b,c))
+print ("The converted value from radians to degrees is : ", end="")
+print(math.degrees(a))
+print ("The converted value from degrees to radians is : ", end="")
+print(math.radians(b))
